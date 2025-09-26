5bca0acb2a2cd8df03211db2eca0b10f4bd4b461
Jihwan Hong <csjihwanh@gmail.com>
2025-09-26 12:14:16 +0900

    feat: added markdown table package

--- src/components/markdown-renderer.js
+++ src/components/markdown-renderer.js
@@ -2,24 +2,19 @@
 
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 
 function Anchor({ node, ...props }) {
-  return (
-    <a
-      {...props}
-      className="text-blue-700 hover:text-blue-800 underline"
-    />
-  );
+  return <a {...props} className="text-blue-700 hover:text-blue-800 underline" />;
 }
 
-export default function MarkdownRenderer({ content }) {
+export default function MarkdownRenderer({ content }: { content: string }) {
   return (
     <div className="markdown prose lg:prose-xl mx-auto mb-10">
       <ReactMarkdown
-        rehypePlugins={[rehypeRaw]}
-        components={{
-          a: Anchor,
-        }}
+        remarkPlugins={[remarkGfm]}     // <-- enables tables, task lists, autolinks, etc.
+        rehypePlugins={[rehypeRaw]}     // <-- allows <br /> inside cells (raw HTML)
+        components={{ a: Anchor }}
       >
         {content}
       </ReactMarkdown>
